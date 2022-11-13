ab27c05fac5a4c13c329aec8e812ad984ade7682
Asma <as1588ma@gmail.com>
2022-11-12 22:51:33 -0500

    complete homework

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
+    def name=(gypsy)
+        @this_dogs_name = gypsy
 
+    end
+
+    def name
+        @this_dogs_name
+    end
 end
+
+class  Cat
+    def pretty_name= (name)
+        @name_instance = name
+    end
+    def pretty_name
+        @name_instance
+    end
+end
+
+jupiter = Dog.new
+jupiter.name = "Jupiter"
+
+puts jupiter.name
+gypsy = Cat.new
+gypsy.pretty_name = "mrs gypsy"
+
+puts gypsy.pretty_name
